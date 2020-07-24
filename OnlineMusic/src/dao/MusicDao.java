b508392230f222b6fd47509f8f4f6c3b28eb59c9
Josvin <17868876191@163.com>
2020-07-24 12:13:19 +0800

    web

---
+++ OnlineMusic/src/dao/MusicDao.java
@@ -0,0 +1,12 @@
+package dao;
+
+/**
+ * Created With IntelliJ IDEA .
+ *
+ * @author Josvin
+ * description:
+ * path: OnlineMusic-dao-MusicDao
+ * date: 2020/7/23 23:24
+ */
+public class MusicDao {
+}
---
+++ OnlineMusic/src/dao/UserDao.java
@@ -0,0 +1,12 @@
+package dao;
+
+/**
+ * Created With IntelliJ IDEA .
+ *
+ * @author Josvin
+ * description:
+ * path: OnlineMusic-dao-UserDao
+ * date: 2020/7/23 23:24
+ */
+public class UserDao {
+}
