89e846ac7942793602017af7a2d52ec10e5957e3
Josvin <17868876191@163.com>
2020-08-07 23:57:43 +0800

    2021 京东笔试

--- java_niuke/src/java_20200807/Test2.java
+++ java_niuke/src/java_20200807/Test2.java
@@ -4,43 +4,43 @@ package java_20200807;
 import java.util.*;
 public class Test2 {
     public static void main(String[] args) {
-        try (Scanner in = new Scanner(System.in)) {
-            System.out.println(helper(in.nextInt(), in.nextInt()));
-        }
+        Scanner in = new Scanner(System.in);
+        System.out.println(helper(in.nextInt(), in.nextInt()));
+        /*这个代码可以检验删数的结果
+        for (int i = 0; i < 6; i++) {
+            int ans = toDelOne("123456", i);
+            System.out.println(ans);
+        }*/
     }
 
     public static int helper(int l, int r) {
-
         int count = 0;
-        if (l == 1) {
-            l++;//1不是素数
-        }
-        while (l <= r) {
+        while (l < r) {
             String str = String.valueOf(l);
             for (int i = 0; i < str.length(); i++) {
                 int ans = toDelOne(str, i);
-                if (isHuiWen(l) && isSu(l)) {
+                //注意1的判断
+                if (l != 1 && isHuiWen(l) && isSu(l)) {
                     count++;
+                    System.out.println(l);
                     break;
-                } else if (isHuiWen(ans) && isSu(ans)) {
+                } else if (ans != 1 && isHuiWen(ans) && isSu(ans)) {
                     count++;
-                    continue;
+                    System.out.println(ans);
+                    break;
                 }
             }
             l++;
-
         }
         return count;
     }
-
-
-
+    //删除str的第i位
     private static int toDelOne(String str, int i) {
         int num = Integer.parseInt(str);
-        int ans = (int) (num/Math.pow(10,i)*Math.pow(10,i) + num%Math.pow(10,i));
+        int ans = (int) ((int) (num/Math.pow(10,i +1 ))*Math.pow(10,i  ) + num%Math.pow(10,i  ));
         return ans;
     }
-
+    //判断素数
     public static boolean isSu(int num){
         int i = 2;
         while(i * i <= num){
@@ -52,6 +52,7 @@ public class Test2 {
         }
         return true;
     }
+    // 判断回文
     public static boolean isHuiWen(int num){
         char[] cs = Integer.toString(num).toCharArray();
         int i = 0,j = cs.length - 1;
