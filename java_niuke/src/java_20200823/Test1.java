7e2ae991f491761e233d1ccd860e0120ab96c122
Josvin <17868876191@163.com>
2020-08-23 21:26:30 +0800

    11

---
+++ java_niuke/src/java_20200823/Test1.java
@@ -0,0 +1,46 @@
+package java_20200823;
+
+/**
+ * @ Created with IntelliJ IDEA.
+ * @ClassName Test1
+ * @Description
+ * @Author by小房
+ * @Date 2020/8/23 20:33
+ */
+//节点
+class Node{
+    public int value;
+    public Node next;
+    public Node(int data) {
+        this.value = data;
+    }
+}
+
+public class Test1 {
+    public Node removeLastKthNode(Node head, int K) {
+        if (head == null || K < 1)
+            return head;
+        Node temp = head;
+        int num = 0;
+        while (temp != null) {
+            num++;
+            temp = temp.next;
+        }
+        if (num == K) {
+            return head.next;
+        }
+        if (num > K) {
+            temp = head;
+            //删除第(num-k+1)个节点
+            //定位到这个点的前驱
+            while (num - K != 0) {
+                temp = temp.next;
+                num--;
+            }
+            temp.next = temp.next.next;
+        }
+        return head;
+    }
+
+    
+}
