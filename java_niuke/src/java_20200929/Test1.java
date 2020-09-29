12c9d748e8a02c4bebe772c59ef5b71b631a726d
Josvin <17868876191@163.com>
2020-09-29 23:28:46 +0800

    字符串分割

---
+++ java_niuke/src/java_20200929/Test1.java
@@ -0,0 +1,35 @@
+package java_20200929;
+
+import java.util.Set;
+
+/**
+ * @ Created with IntelliJ IDEA.
+ * @ClassName Test1
+ * @Description
+ * @Author by小房
+ * @Date 2020/9/29 23:10
+ */
+public class Test1 {
+    public class Solution {
+        public boolean wordBreak(String s, Set<String> dict) {
+            boolean[] ans = new boolean[s.length() + 1];
+            //ans[0] = true;
+
+            for (int i = 1; i <= s.length(); i++) {
+
+                if (dict.contains(s.substring(0,i))) {
+                    ans[i] = true;
+                    continue;
+
+                }
+                for (int j = i - 1; j >= 0; j--) {
+                    if (ans[j] &&  dict.contains(s.substring(j,i))) {
+                        ans[i] = true;
+                        break;
+                    }
+                }
+            }
+            return ans[s.length()];
+        }
+    }
+}
